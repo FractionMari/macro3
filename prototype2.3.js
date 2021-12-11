262b4b536a95f9dbdfaa197f119508ab6274e1bf
FractionMari <lesteberg@gmail.com>
2021-12-11 12:43:38 +0100

    commenting

--- prototype2.3.js
+++ prototype2.3.js
@@ -490,17 +490,18 @@ const harmNotes = [-12, -11, -8, -6, -4, -3, -2]
      randomArray6.push(random6);
 
      // getting random numbers
-     let random4 = getRandomInt(10);
+     let random4 = getRandomInt(11);
      let random5 = getRandomInt(14);
 
 
-     // Generating random rhythm groove and random melody. Selecting notes from the arrays random, random2 and random6
-     if (random4 < 4)
-     randomHiHatArray.push(("C1 C1").split(" ")),
-     randomMelodyArray.push(random);
+     // Generating random rhythm groove and random melody. Selecting notes from the arrays random, random2, random3 and random6
+
      if (random4 == 1)
      randomHiHatArray.push(("C1 C1").split(" ")),
      randomMelodyArray.push((0 + " " + random).split(" "));
+     if (random4 < 4)
+     randomHiHatArray.push(("C1 C1").split(" ")),
+     randomMelodyArray.push(random);
      if (random4 == 7)
      randomHiHatArray.push(("C2 C2").split(" ")),
      randomMelodyArray.push((random + " " + random6).split(" "));
@@ -511,11 +512,11 @@ const harmNotes = [-12, -11, -8, -6, -4, -3, -2]
 
      if (random4 == 5)
      randomHiHatArray.push("C1"),
-     randomMelodyArray.push((random3 + random6).split(" "));
+     randomMelodyArray.push((random + " " + random6).split(" "));
 
      if (random4 == 4)
      randomHiHatArray.push("C1"),
-     randomMelodyArray.push(random3);
+     randomMelodyArray.push((0 + " " + random).split(" "));
 
      if (random4 == 8)
      randomHiHatArray.push(("C1 C2").split(" ")),
@@ -524,6 +525,10 @@ const harmNotes = [-12, -11, -8, -6, -4, -3, -2]
      if (random4 == 9)
      randomHiHatArray.push((" " + "C2").split(" ")),
      randomMelodyArray.push((random6 + " " + random2).split(" "));
+
+     if (random4 == 10)
+     randomHiHatArray.push("C1"),
+     randomMelodyArray.push((random + " " + random2 + " " + random6).split(" "));
 
      else
      randomHiHatArray.push("C1"),
@@ -544,21 +549,11 @@ const harmNotes = [-12, -11, -8, -6, -4, -3, -2]
   };
 
 
-  //console.log(randomDrumArray);
-
-
-
   }
 
 
 
-
-
-// ["C1", "C1", "E4", ["C2", "C2"], "C1", "C1", "F2", "F2"]
-
-/// DiffCam Variables:
-
-
+/// DiffCam Variables for Motion Capture:
 
 var DiffCamEngine = (function() {
 
